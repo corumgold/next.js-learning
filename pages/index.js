5693218a80da63fb7724a0015a848f5ae774d89f
Corum Gold <goldcorum@gmail.com>
2022-10-07 12:44:37 +0200

    Add head for SEO

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      {/* Head is for SEO */}
+      <Head>
+        <title>Web Dev Newz</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
       <h1>Welcome to Next</h1>
     </div>
-  )
+  );
 }
